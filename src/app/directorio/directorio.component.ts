ea9c3500266b0dfabe4794664a3d55611d2ac8c2
Antonio Adame <a.adame59@yahoo.com>
2021-02-08 11:54:25 -0600

    -detect changes on delegacionId input and set form value to that

--- src/app/directorio/directorio.component.ts
+++ src/app/directorio/directorio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, OnChanges, SimpleChanges } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { SearchForm } from '../data/search-form';
 
@@ -7,9 +7,9 @@ import { SearchForm } from '../data/search-form';
   templateUrl: './directorio.component.html',
   styleUrls: ['./directorio.component.css']
 })
-export class DirectorioComponent implements OnInit {
+export class DirectorioComponent implements OnChanges {
 
-  @Input() delegacionId! : String;
+  @Input() delegacionId! : string;
   @Input() minified : boolean = false;
 
   //keep default copy of data in case we need to reset it
@@ -22,19 +22,13 @@ export class DirectorioComponent implements OnInit {
 
   constructor() { }
 
-<<<<<<< HEAD
   searchAsociados(form: NgForm, event : any) {
     console.log(this.searchForm);
-    this.searchForm = { ...this._searchForm };
-=======
-  searchAsociados(f: NgForm, event : any) {
-    event.preventDefault();
-
-    let delegacion_id = (f.value.delegacionId)?f.value.delegacionId:this.delegacionId;
->>>>>>> 7223f54c35edf78f0af94aa68aeaa17b2b7751f2
-  }
-  
-  ngOnInit(): void {
   }
 
+  //detect changes on input attributes
+  ngOnChanges($changes : SimpleChanges) {
+    //on map selection, set delegacionId of form
+    this.searchForm.delegacionId = $changes.delegacionId.currentValue;
+  }
 }
